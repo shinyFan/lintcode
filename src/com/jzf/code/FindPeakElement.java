29e7973d0dc7a61856430fe76df57458321bc3ed
Pizi <724339674@qq.com>
2019-03-27 19:16:25 +0800

    3月27日

--- src/com/jzf/code/FindPeakElement.java
+++ src/com/jzf/code/FindPeakElement.java
@@ -1,12 +1,6 @@
 package com.jzf.code;
 
 public class FindPeakElement {
-    public static void main(String[] args) {
-
-    }
-
-
-
     public int findPeakElement(int[] nums){
         int length = nums.length;
         if(length==0){
