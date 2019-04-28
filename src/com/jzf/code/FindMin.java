03e9690013e8e1be72dcaaa5b5d2abb7a47bb4d7
jinzongfan <jinzongfan@yuantu.onaliyun.com>
2019-04-28 14:11:18 +0800

    4月26日 交叉

---
+++ src/com/jzf/code/FindMin.java
@@ -0,0 +1,22 @@
+package com.jzf.code;
+
+public class FindMin {
+    public int findMin(int[] nums) {
+        // write your code here
+        
+        int left = 0;
+        int right = nums.length-1;
+        int mid;
+
+        while (left < right) {
+            mid = left + ((right - left) >>2);
+            if (nums[mid] > nums[right]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+
+        return nums[left];
+    }
+}
---
+++ src/com/jzf/code/Sqrt.java
@@ -0,0 +1,31 @@
+package com.jzf.code;
+
+public class Sqrt {
+
+    public int sqrt(int x) {
+        // write your code here
+        int result=1;
+        if(x<1){
+            return 0;
+        }
+        if(x==Integer.MAX_VALUE){
+            return 46340;
+        }
+        while (result*result<x+1){
+            result<<=1;
+        }
+        //平方根在result和result/2之间；
+        int lo=result>>1;
+        int hi=result;
+        int mid=lo+((hi-lo)>>1);
+        while (lo+1<hi){
+            if(mid*mid<x){
+                lo=mid;
+            }else{
+                hi=mid-1;
+            }
+            mid=lo+((hi-lo)>>1);
+        }
+        return mid;
+    }
+}
